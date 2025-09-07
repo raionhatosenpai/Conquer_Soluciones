475755594729c78004e3819b97630adc65927590
raionhatosenpai <luiscarlosquinchiacortes@gmail.com>
2025-09-07 11:37:15 +0200

    Cambios 07/09/2025

--- Python_Avanzado.py/Ejercicios2.py/Validacion_formulario.py
+++ Python_Avanzado.py/Ejercicios2.py/Validacion_formulario.py
@@ -7,19 +7,24 @@
 # 2. Que el teléfono este conformado por dígitos y tenga una longitud de 9 caracteres
 # 3. Que el email contenga un “@“ y un “.”
 
-def validar_formulario(nombre, email, telefono):
-    if len(nombre) < 3:
+def validar_formulario(Nombre, Email, Telefono):
+    if len(Nombre) < 3:
         return "El nombre debe tener al menos 3 caracteres."
-    if not telefono.isdigit() or len(telefono) != 9:
+    if not Telefono.isdigit() or len(Telefono) != 9:
         return "El teléfono debe tener 9 dígitos."
-    if "@" not in email or "." not in email:
+    if "@" not in Email or "." not in Email:
         return "El email debe contener un '@' y un '.'."
     return "Formulario válido."
 
 # Ejemplos de uso
-print(validar_formulario("Ana", "ana@example.com", "123456789"))
-print(validar_formulario("Jose", "jose@examplecom", "12345689"))
-print(validar_formulario("Luis", "luis@example.com", "123456789"))
-print(validar_formulario("Al", "al@example.com", "123456789"))
-print(validar_formulario("Maria", "maria@example.com", "123456789"))
-print(validar_formulario("Marta", "marta@examplecom", "123456789"))
+Nombre = input("Introduce tu nombre: ")
+Email = input("Introduce tu email: ")
+Telefono = input("Introduce tu teléfono: ")
+print(validar_formulario(Nombre, Email, Telefono))
+
+# print(validar_formulario("Ana", "ana@example.com", "123456789"))
+# print(validar_formulario("Jose", "jose@examplecom", "12345689"))
+# print(validar_formulario("Luis", "luis@example.com", "123456789"))
+# print(validar_formulario("Al", "al@example.com", "123456789"))
+# print(validar_formulario("Maria", "maria@example.com", "123456789"))
+# print(validar_formulario("Marta", "marta@examplecom", "123456789"))
