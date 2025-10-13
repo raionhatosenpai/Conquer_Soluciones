d4f24295297521fb2b1990de26a92dbd58b8e1b8
Raionhato <luiscarlosquinchiacortes@gmail.com>
2025-10-13 21:36:07 +0200

    Cambios 13/10/2025

---
+++ Python_Avanzado.py/Ejercicios7.py/Problema_organizacion_datos_empresariales.py
@@ -0,0 +1,9 @@
+# Tu tarea es organizar una lista consolidada de todos los empleados de la 
+# empresa. La organización debe seguir ciertas reglas:
+# -Los empleados se deben ordenar por el rendimiento en proyectos recientes 
+# de forma descendente.
+# -Para aquellos con el mismo rendimiento, se deben ordenar por edad de 
+# forma ascendente. Además, deseas agrupar a los empleados por país para 
+# un análisis más efectivo. Utiliza funciones lamba.
+
+# 
